0d4e761d62724f32182ba5bd48ead98516467145
alexv999 <alexander.vasiliev24@ycdsbk12.ca>
2024-06-13 22:43:19 -0400

    added bubble sort (will be used in shop)

---
+++ Assets/Scripts/BubbleSort.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BubbleSort : MonoBehaviour
+{
+    public static void Main(string[] args)
+    { 
+        int[] a = { 1, 0, 2, 3 }; // array example
+        int t; // temp num
+
+        for (int p = 0; p <= a.Length - 2; p++) // outer loop for repetition
+        {
+            for (int i = 0; i <= a.Length - 2; i++) // inner loop for swapping
+            {
+                if (a[i] > a[i + 1]) // check
+                {
+                    t = a[i + 1]; // swap
+                    a[i + 1] = a[i];
+                    a[i] = t;
+                }
+            } 
+        }
+
+
+    }
+}
